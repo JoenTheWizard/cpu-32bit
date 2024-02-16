a0ba591100c21709ce223d2c7a51095fc6945029
joenwiz <joenthewizard0@gmail.com>
2024-02-16 12:13:03 +1100

    4 input ALU with 16 bit input/output values

---
+++ ALU16bit/ALU16bit.v
@@ -0,0 +1,24 @@
+module ALU16bit(
+  input [15:0] a, b,
+  input sel0, sel1,
+  output [15:0] out
+);
+
+//Multiplexer
+wire [15:0] operation1 = a + b;
+wire [15:0] operation2 = a - b;
+wire [15:0] operation3 = a & b;
+wire [15:0] operation4 = a | b;
+
+//assign out[0] =  (operation1[0] & (~sel0 & ~sel1)) | (operation2[0] & (sel0 & ~ sel1)) | (operation3[0] & (~sel0 & sel1)) | (operation4[0] & (sel0 & sel1));
+//assign out[1] =  (operation1[1] & (~sel0 & ~sel1)) | (operation2[1] & (sel0 & ~ sel1)) | (operation3[1] & (~sel0 & sel1)) | (operation4[1] & (sel0 & sel1));
+//assign out[2] =  (operation1[2] & (~sel0 & ~sel1)) | (operation2[2] & (sel0 & ~ sel1)) | (operation3[2] & (~sel0 & sel1)) | (operation4[2] & (sel0 & sel1));
+//assign out[3] =  (operation1[3] & (~sel0 & ~sel1)) | (operation2[3] & (sel0 & ~ sel1)) | (operation3[3] & (~sel0 & sel1)) | (operation4[3] & (sel0 & sel1));
+
+genvar i;
+
+for (i = 0; i < 16; i=i+1) begin   
+  assign out[i] =  (operation1[i] & (~sel0 & ~sel1)) | (operation2[i] & (sel0 & ~ sel1)) | (operation3[i] & (~sel0 & sel1)) | (operation4[i] & (sel0 & sel1));
+end 
+
+endmodule;
---
+++ ALU16bit/ALU16bit_tb.v
@@ -0,0 +1,23 @@
+module ALU16bit_tb;
+
+reg [15:0] a;
+reg [15:0] b;
+reg sel0, sel1;
+wire [15:0] out;
+
+ALU16bit uut(a,b,sel0,sel1,out);
+
+initial begin
+    $dumpfile("build/ALU16bit.vcd"); //Assume in 'build' directory
+    $dumpvars(0, ALU16bit_tb);
+
+    //Apply inputs and check the output
+    a =  20; b =  38; sel1 =  0; sel0 =  0; #10; //Output in1
+    a =  8; b =  5; sel1 =  0; sel0 =  1; #10; //Output in2
+    a =  5; b =  4; sel1 =  1; sel0 =  0; #10; //Output in3
+    a =  6; b =  3; sel1 =  1; sel0 =  1; #10; //Output in4    
+
+    $finish();
+end
+
+endmodule

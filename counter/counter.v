477e41d5c0c6eddd8375d1741a04870657f6a3a7
joenwiz <joenthewizard0@gmail.com>
2024-02-27 10:19:12 +1100

    add 'load' to the counter, so it can act like a program counter

--- counter/counter.v
+++ counter/counter.v
@@ -1,5 +1,6 @@
 module counter(
-  input clk,
+  input clk, 
+  input load, input [3:0] load_val,
   output reg [3:0] count
 );
 
@@ -7,7 +8,11 @@ initial count = 4'b0000;
 
 //assign count = 0;
 always @(posedge clk) begin
-  count <= count + 1;
+  if (load) begin
+    count <= load_val;
+  end else begin
+    count <= count + 1;
+  end
 end
 
 endmodule;
--- counter/counter_tb.v
+++ counter/counter_tb.v
@@ -1,16 +1,30 @@
 module counter_test;
 
-reg clk = 0;
+reg clk = 0, load = 0;
+//reg load =  0;
+reg [3:0] load_val =  4'b0000;
 wire [3:0] count;
 
-counter uut(.clk(clk), .count(count));
+counter uut(.clk(clk), .load(load), .load_val(load_val), .count(count));
 
-always #1 clk = ~clk; //every 1 sec
+always #1 clk = ~clk; //every 1 cycle
 
 initial begin
     $dumpfile("build/counter.vcd"); //Assume in 'build' directory
     $dumpvars(0, counter_test);
-    
+   
+    //Simulate control unit
+    load_val = 4'h7;
+    #11
+    load = 1;
+    #1 load = 0;
+
+    //Simulate control unit
+    load_val = 4'h4;
+    #11
+    load = 1;
+    #1 load = 0;
+
     #50
 
     $finish();

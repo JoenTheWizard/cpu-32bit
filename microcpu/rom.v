e3ac5cf4beea279b83772d5d8e9f954ecb49794a
JoenTheWizard <joenwiz@localhost.localdomain>
2024-06-06 17:51:37 +1000

    paramaterize rom module

--- microcpu/rom.v
+++ microcpu/rom.v
@@ -1,18 +1,22 @@
-module rom(
+module rom # (
+    parameter ADDR_WIDTH = 13, //No. of addressing bits (must be less than 'address' bit length)
+    parameter DATA_WIDTH = 32  //Width of data bits
+) (
     input      [31:0] address, //Assuming 32-bit address space
-    output reg [31:0] data_out
+    output reg [DATA_WIDTH-1:0] data_out
 );
 
 //Memory size of 8192 words with 32-bit instruction width (2^13 available dwords) 
-reg [31:0] mem[0:8191];
+reg [DATA_WIDTH-1:0] mem[0:(2**ADDR_WIDTH-1)];
 
 //Read from file to initialize the instructions
 initial begin
-   $readmemb("readmem/instructions.mem", mem);
+    $readmemb("readmem/instructions.mem", mem);
 end
 
 always @(*) begin
-    data_out <= mem[address];
+    //Address length will be the masked bits specified by the ADDR_WIDTH
+    data_out <= mem[address[ADDR_WIDTH-1:0]];
 end
 
 endmodule
